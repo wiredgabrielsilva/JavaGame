fa47b6184b9eeba1dc10105864d5a74c65fde626
wiredgabrielsilva <100141597+wiredgabrielsilva@users.noreply.github.com>
2024-02-22 16:51:43 -0300

    Number Game v0.5
    Added a try catch so it doesn't crash when parsing text or null values.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,11 +4,12 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.util.Scanner;
+import javax.swing.SwingConstants;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import java.util.Random;
 
 public class HelloWorld {
@@ -32,44 +33,56 @@ public class HelloWorld {
 			Random rand = new Random();
 			int int_random = rand.nextInt(100);
 
+			// Remember to comment
+			//System.out.println(int_random);
+
 			JFrame frame = new JFrame();
 			JPanel panel = new JPanel();
-			JButton button = new JButton("Make a Guess");
-			JLabel label = new JLabel("Latest Guess:");
+			JButton button = new JButton("Make a guess");
+			JLabel label = new JLabel("Can you find out the random number? HINT: It's from 0 to 100",
+					SwingConstants.CENTER);
 			JLabel guessLabel = new JLabel();
-			JLabel highLow = new JLabel();
+			JLabel highLow = new JLabel("", SwingConstants.CENTER);
 			JTextField textField = new JTextField();
 
 			// Setting the GUI up
-			panel.setBorder(BorderFactory.createEmptyBorder(200, 200, 200, 200));
-			panel.setLayout(new GridLayout());
-			panel.add(button);
-			panel.add(textField);
+			panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
+			panel.setLayout(new GridLayout(5, 5));
 			panel.add(label);
+			panel.add(textField);
+			panel.add(button);
 			panel.add(guessLabel);
 			panel.add(highLow);
 
 			frame.add(panel, BorderLayout.CENTER);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setTitle("Our GUI");
+			frame.setTitle("Number Game");
 			frame.pack();
 			frame.setVisible(true);
 
 			button.addActionListener(new ActionListener() {
+
 				public void actionPerformed(ActionEvent e) {
-					String text = textField.getText();
 
-					int guess = Integer.parseInt(text);
+					try {
+						String text = textField.getText();
+						int guess = Integer.parseInt(text);
 
-					if (guess > int_random) {
-						label.setText("too high");
-					} else if (guess < int_random) {
-						label.setText("too low");
-					} else if (guess == int_random) {
-						label.setText("You won!");
+						if (guess > int_random) {
+							highLow.setText("Too High");
+						} else if (guess < int_random) {
+							highLow.setText("Too Low");
+						} else if (guess == int_random) {
+							highLow.setText("You won!");
+
+						}
+					} catch (Exception r) {
+						highLow.setText("Oops, something went wrong. Please try something else");
 					}
-				}
-			});
+
+				} // actionPerformed
+			} // ActionListener
+			); // ActionListener
 
 		}
 
