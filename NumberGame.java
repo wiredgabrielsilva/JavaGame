abb2504a5433fbff53a23600540c1a7c3beb23fa
wiredgabrielsilva <100141597+wiredgabrielsilva@users.noreply.github.com>
2024-02-22 20:30:22 -0300

    Fixes and new features
    - Added a HEALTH system where you have 20 chances to find out the random number
    - Added a new mechanic where it tells you how close/far you are from the random number
    - Fixed the name from HelloWorld to NumberGame

--- NumberGame.java
+++ NumberGame.java
@@ -6,13 +6,15 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
-public class HelloWorld {
+public class NumberGame {
 
 //	private final GUI gui;
 //	private final Game game;
@@ -28,31 +30,40 @@ public class HelloWorld {
 		public JTextField textField;
 		public JLabel myGuess;
 
-		public GUI(HelloWorld helloworld) {
+		public GUI(NumberGame helloworld) {
 
 			Random rand = new Random();
 			int int_random = rand.nextInt(100);
 
 			// Remember to comment
-			//System.out.println(int_random);
+			// System.out.println(int_random);
 
 			JFrame frame = new JFrame();
 			JPanel panel = new JPanel();
+			JPanel bottomPanel = new JPanel();
+
 			JButton button = new JButton("Make a guess");
 			JLabel label = new JLabel("Can you find out the random number? HINT: It's from 0 to 100",
 					SwingConstants.CENTER);
 			JLabel guessLabel = new JLabel();
 			JLabel highLow = new JLabel("", SwingConstants.CENTER);
-			JTextField textField = new JTextField();
+			JTextField textField = new JTextField("", SwingConstants.CENTER);
+			JLabel lifeBar = new JLabel("HEALTH: ", SwingConstants.RIGHT);
+			JLabel lifeTotal = new JLabel("100");
 
 			// Setting the GUI up
-			panel.setBorder(BorderFactory.createEmptyBorder(100, 100, 100, 100));
-			panel.setLayout(new GridLayout(5, 5));
+			panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 1, 10));
+			panel.setLayout(new GridLayout(0, 1, 0, 10));
 			panel.add(label);
 			panel.add(textField);
 			panel.add(button);
 			panel.add(guessLabel);
 			panel.add(highLow);
+			panel.setAlignmentX(Component.LEFT_ALIGNMENT);
+			bottomPanel.setBorder(BorderFactory.createEmptyBorder(5, 30, 30, 30));
+			panel.add(bottomPanel, BorderLayout.PAGE_END);
+			bottomPanel.add(lifeBar);
+			bottomPanel.add(lifeTotal);
 
 			frame.add(panel, BorderLayout.CENTER);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -61,23 +72,56 @@ public class HelloWorld {
 			frame.setVisible(true);
 
 			button.addActionListener(new ActionListener() {
+				int life = 100;
 
 				public void actionPerformed(ActionEvent e) {
+					life = life - 5;
+					String setLife = String.valueOf(life);
+					lifeTotal.setText(setLife);
 
 					try {
 						String text = textField.getText();
 						int guess = Integer.parseInt(text);
+						int trial = Math.abs(int_random - guess);
+						// debug
+						// System.out.println(trial);
 
 						if (guess > int_random) {
-							highLow.setText("Too High");
-						} else if (guess < int_random) {
-							highLow.setText("Too Low");
-						} else if (guess == int_random) {
-							highLow.setText("You won!");
+							if (trial > 0 && trial < 5 && trial != int_random) {
+								highLow.setText("Almost!!!");
+							} else if (trial > 0 && trial < 15) {
+								highLow.setText("High but close");
+							} else if (trial > 0 && trial < 30) {
+								highLow.setText("High");
+							} else {
+								highLow.setText("Too High");
+							}
 
+						} else if (guess < int_random) {
+
+							if (trial > 0 && trial < 5 && guess != int_random) {
+								highLow.setText("Almost!!!");
+							} else if (trial > 0 && trial < 15) {
+								highLow.setText("Low but close");
+							} else if (trial > 0 && trial < 30) {
+								highLow.setText("Low");
+							} else {
+								highLow.setText("Too Low");
+							}
+						} else {
+							highLow.setText("You won!!!!");
+							button.setEnabled(false);
 						}
+
 					} catch (Exception r) {
-						highLow.setText("Oops, something went wrong. Please try something else");
+						life = life + 5;
+						highLow.setText("Oops, something went wrong. Please try something else.");
+					}
+
+					if (life == 0) {
+						highLow.setText("GAME OVER");
+						lifeTotal.setText("GAME OVER");
+						button.setEnabled(false);
 					}
 
 				} // actionPerformed
@@ -90,9 +134,9 @@ public class HelloWorld {
 
 	public static void main(String args[]) {
 
-		HelloWorld helloworld = new HelloWorld();
+		NumberGame helloworld = new NumberGame();
 
-		HelloWorld.GUI gui = helloworld.new GUI(helloworld);
+		NumberGame.GUI gui = helloworld.new GUI(helloworld);
 	}
 
 }
